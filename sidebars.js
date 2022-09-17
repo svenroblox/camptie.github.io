977db956436636221a4c6abfce0b960c0baf8176
svenroblox <111045168+svenroblox@users.noreply.github.com>
2022-09-17 17:00:32 +0930

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
 	sidebar: {
-		Camptie: ["camptie/introduction"],
+		Camptie: ["camptie/introduction","camptie/inGame"],
 		Guides: ["Guides/permissionOverrides", "Guides/customBotAccount"],
 		API: ["API/gettingStarted", "API/group"],
 	},
